bf8c604a372f1accb8ae447562b12b1403a4b6e3
Marc Karassev <karassev.marc@etu.unice.fr>
2015-05-22 11:26:53 +0200

    Demonstration menu.

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,4 +1,5 @@
 import javax.jms.JMSException;
+import java.util.Scanner;
 
 /**
  * @author Marc Karassev
@@ -8,15 +9,37 @@ import javax.jms.JMSException;
 public class Client {
 
     private MiniTwitterClient miniTwitterClient;
+    private Scanner scanner;
 
     public Client() throws JMSException {
         miniTwitterClient = new MiniTwitterClient();
+        scanner = new Scanner(System.in);
     }
 
     /**
      * Runs a demonstration scenario.
      */
     public void runDemonstration() throws JMSException {
+        boolean run = true;
+        System.out.println("Welcome to MiniTwitter!");
+        while (run) {
+            System.out.println("What do you want to do?\n1 - Post a tweet\n2 - Quit");
+            switch (scanner.nextInt()) {
+                case 1:
+                    scanner.nextLine();
+                    tweet();
+                    break;
+                case 2:
+                    scanner.nextLine();
+                    run = false;
+                    break;
+                default:
+            }
+        }
+    }
+
+    // allows the user to tweet
+    private void tweet() throws JMSException {
         miniTwitterClient.sendMessage("Hello!");
     }
 
